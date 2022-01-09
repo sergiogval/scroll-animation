2fdccd945b3c1395122f6612b666fc4c5dda15cf
sergiogval <sergioval@gmail.com>
2022-01-09 17:55:53 -0600

    Add an animation to show boxes when scroll

---
+++ index.js
@@ -0,0 +1,18 @@
+const boxes = document.querySelectorAll('.box')
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes()
+
+function checkBoxes() {
+  const triggerPoint = window.innerHeight / 5 * 4
+
+  boxes.forEach(box => {
+    boxTop = box.getBoundingClientRect().top
+
+    if (boxTop < triggerPoint) {
+      box.classList.add('show')
+    } else {
+      box.classList.remove('show')
+    }
+})}
